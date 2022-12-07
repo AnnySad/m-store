b0aae38aaefdcce2b9397e9e2e3207c470453ccb
AnnySad <anna.sadovskaya555@gmail.com>
2022-12-07 20:25:18 +0300

    modal-create-new-pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Product} from "./components/Product";
 import {useProducts} from "./hooks/products";
 import Loader from "./components/Loader";
 import {ErrorMessage} from "./components/Error";
 import Modal from "./components/Modal";
 import CreatProductModal from "./components/CreatProductModal";
+import {IProduct} from "./modelsType";
 
 
 function App() {
 
-const {loading, error, products} = useProducts()
+    const {loading, error, products, addProduct} = useProducts()
+    const [modal, setModal] = useState(false)
+
+const createHandler = (product:IProduct) => {
+  setModal(false)
+    addProduct(product)
+}
+
     return (
         <div className="container mx-auto max-w-2xl pt-5">
             {loading && <Loader/>}
-            {error && <ErrorMessage error = {error}/>}
+            {error && <ErrorMessage error={error}/>}
             {products.map(prod => <Product product={prod} key={prod.id}/>)}
 
-            <Modal title="Creat new product">
-                <CreatProductModal/>
-                </Modal>
+            {modal && <Modal title="Creat new product" onClose={()=> setModal(false)}>
+                <CreatProductModal onCreate={createHandler}/>
+            </Modal>}
+
+            <button className='fixed bottom-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+            onClick={() => setModal(true)}
+            >+</button>
         </div>
 
     )
--- src/components/CreatProductModal.tsx
+++ src/components/CreatProductModal.tsx
@@ -1,29 +1,60 @@
 import React, {useState} from 'react';
+import {IProduct} from "../modelsType";
+import axios from "axios";
+import {ErrorMessage} from "./Error";
 
 
+const productData: IProduct = {
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 42,
+        count: 10
+    }
+}
 
-const CreatProductModal = () => {
+interface CreateProProps {
+    onCreate: (product:IProduct) => void
+}
+
+const CreatProductModal = ({onCreate}:CreateProProps) => {
 
     const [value, setValue] = useState('')
+    const [error, setError] = useState('')
 
-   const submitHandler = (event:React.FormEvent) => {
-       event.preventDefault()
-   }
+    const submitHandler = async (event: React.FormEvent) => {
+        event.preventDefault()
+        setError('')
 
-   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-       setValue(event.currentTarget.value)
-   }
+        if (value.trim().length === 0) {
+            setError('Please enter valid title')
+            return
+        }
+
+        productData.title = value
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+
+        onCreate(response.data)
+    }
+
+    const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.currentTarget.value)
+    }
 
     return (
         <form onSubmit={submitHandler}>
             <input
-            type='text'
-            className='border py-2 px-4 mb-2 w-full outline-0'
-            placeholder='Enter product title...'
-            value={value}
-            onChange={changeHandler}
+                type='text'
+                className='border py-2 px-4 mb-2 w-full outline-0'
+                placeholder='Enter product title...'
+                value={value}
+                onChange={changeHandler}
             />
 
+            {error && <ErrorMessage error={error}/>}
             <button type='submit' className='py-2 px-4 border bg-blue-500 hover:text-amber-100'>Create</button>
         </form>
     );
