cdc9dac481869a74974a7fb52e4d44881a8d2b5e
AnnySad <anna.sadovskaya555@gmail.com>
2022-12-07 18:38:27 +0300

    Loading...

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,13 @@ import {IProduct} from "./modelsType";
 function App() {
 
     const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
 
     async function fetchProducts() {
+        setLoading(true)
        const response=await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=8')
         setProducts(response.data)
+        setLoading(false)
     }
 
     useEffect(()=>{
@@ -19,6 +22,7 @@ function App() {
     },[])
    return (
        <div className="container mx-auto max-w-2xl pt-5">
+           {loading && <p className='text-center'>Loading...</p>}
            {products.map(prod => <Product product={prod} key={prod.id}/>)}
        </div>
 
