0cf95c24038a9c6abdaaae22ec2609eaf4767ecc
AnnySad <anna.sadovskaya555@gmail.com>
2022-12-07 17:07:47 +0300

    creat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {createElement as e} from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return e('div',{className:'container'},[
+    e('h1',{className:'fond-bold'}, 'test JSX'),
+      e('button',{className:'btn'}, 'Click me !')
+  ])
 }
 
 export default App;
